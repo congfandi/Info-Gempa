9c88e159acf386b5f442d2b06987ada6c5210ae7
Cong Fandi <congfandi>
2019-11-30 06:27:58 +0700

    Initial Commit

---
+++ QuakesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuakesApp
+//
+//  Created by Cong Fandi on 30/11/19.
+//  Copyright © 2019 Cong Fandi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
